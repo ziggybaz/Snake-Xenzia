6c97992e088a2b8b24a7773cfbfd8848b612a0ef
ZiggyBaz <basil99magawi@gmail.com>
2025-01-08 02:45:20 +0300

    set up dynamic terminal resizing

--- xenzia/src/game.rs
+++ xenzia/src/game.rs
@@ -4,27 +4,22 @@ use termion::{ raw::IntoRawMode, event::{ Key }, clear, cursor, input::TermRead 
 use std::{io::{ stdout, stdin, Read, Write, BufWriter}, collections::VecDeque, time::{ Instant, Duration}, thread::sleep };
 use rand::Rng;
 
-const GRID_WIDTH:u16 = 40;
-const GRID_HEIGHT:u16 = 20;
 
-fn game_grid(stdout: &mut std::io::Stdout) {
+fn game_grid(stdout: &mut std::io::Stdout, width: u16, height: u16) {
     write!(stdout, "{}", cursor::Goto(1, 1)).expect("Unable to position cursor to draw top border");
     print!("+");
-    for _ in 0..GRID_WIDTH {
+    for _ in 0..width {
         print!("=");
     }
     print!("+");
 
-    for y in 2..=GRID_HEIGHT + 1 {
-        write!(stdout, "{}", cursor::Goto(1, y)).expect("Unable to position cursor to draw side border");
-        print!("#");
-        write!(stdout, "{}", cursor::Goto(GRID_WIDTH + 2, y)).expect("fuck, i'll add proper error-handling, tired with thess expect blocks, will just use unwraps as placeholders fo now");
-        println!("#");
+    for y in 2..=height + 1 {
+        write!(stdout, "{}#", cursor::Goto(1, y)).expect("Unable to position cursor to draw side border");
+        write!(stdout, "{}#", cursor::Goto(width + 2, y)).expect("fuck, i'll add proper error-handling, tired with thess expect blocks, will just use unwraps as placeholders fo now");
     }
 
-    write!(stdout, "{}", cursor::Goto(1, GRID_HEIGHT + 2)).unwrap();
-    print!("+");
-    for _ in 0..GRID_WIDTH {
+    write!(stdout, "{}", cursor::Goto(1, height + 2)).unwrap();
+    for _ in 0..width {
         print!("=");
     }
     println!("+");
@@ -37,11 +32,11 @@ fn draw_food(stdout: &mut std::io::Stdout, x: u16, y: u16) {
     print!("*");
 }
 
-fn food_position(x: &mut u16, y: &mut u16, snake: &VecDeque<(u16, u16)>) {
+fn food_position(x: &mut u16, y: &mut u16, snake: &VecDeque<(u16, u16)>, width: u16, height: u16) {
     let mut rng = rand::thread_rng();
     loop {
-        *x = rng.gen_range(2..=GRID_WIDTH);
-        *y = rng.gen_range(2..=GRID_HEIGHT);
+        *x = rng.gen_range(2..=width);
+        *y = rng.gen_range(2..=height);
 
         if !snake.contains(&(*x, *y)) {
             break;
@@ -78,10 +73,10 @@ fn snake_movement(snake: &mut VecDeque<(u16, u16)>, direction: Direction) {
     snake.pop_back();
 }
 
-fn detect_collision(snake: &VecDeque<(u16, u16)>) -> bool {
+fn detect_collision(snake: &VecDeque<(u16, u16)>, width: u16, height: u16) -> bool {
     let head = *snake.front().unwrap();
 
-    if head.0 < 2 || head.0 > GRID_WIDTH + 1 || head.1 < 2 || head.1 > GRID_HEIGHT + 1 {
+    if head.0 < 2 || head.0 > width + 1 || head.1 < 2 || head.1 > height + 1 {
         return true;
     }
 
@@ -92,19 +87,123 @@ fn detect_collision(snake: &VecDeque<(u16, u16)>) -> bool {
     false
 }
 
-fn consume_food(snake: &mut VecDeque<(u16, u16)>, food_x: &mut u16, food_y: &mut u16, stdout: &mut std::io::Stdout) {
+fn consume_food(
+    snake: &mut VecDeque<(u16, u16)>,
+    food_x: &mut u16, food_y: &mut u16,
+    stdout: &mut std::io::Stdout,
+    width: u16,
+    height: u16) {
     let head = *snake.front().unwrap();
 
     if head == (*food_x, *food_y) {
         let tail = *snake.back().unwrap();
         snake.push_back(tail);
 
-        food_position(food_x, food_y, snake);
+        food_position(food_x, food_y, snake, width, height);
         draw_food(stdout, *food_x, *food_y);
     }
 }
 
-fn game_loop() {
+fn display_text(stdout: &mut std::io::Stdout, lines: &[&str]) {
+    let terminal_size = termion::terminal_size().unwrap();
+    let terminal_width = terminal_size.0;
+    let terminal_height = terminal_size.1;
+
+    let start_y = (terminal_height / 2) - (lines.len() as u16 / 2);
+    for (i, line) in lines.iter().enumerate() {
+        let x = (terminal_width / 2) - (line.len() as u16 / 2);
+        write!(stdout, "{}{}", cursor::Goto(x, start_y + i as u16), line).unwrap();
+    }
+    stdout.flush().unwrap();
+}
+
+fn welcome_screen() {
+    let stdout = stdout();
+    let mut raw_stdout = stdout.into_raw_mode().unwrap();
+
+    write!(raw_stdout, "{}{}", clear::All, cursor::Hide).unwrap();
+
+    let welcome_text = [
+        "Welcome To SNAKE XENZIA", // i know the name is trademarked but I'm still going to use it,
+        "",
+        "INSTRUCTIONS:",
+        " - Use Arrow Keys to move the snake.",
+        " - Collect the food to grow your snake.",
+        " - Avoid colliding with the borders and crushing into yourself",
+        "",
+        "Press 'ENTER' to start..."
+    ];
+
+    display_text(&mut raw_stdout, &welcome_text);
+
+    let stdin = stdin();
+    for key in stdin.keys() {
+        if let Ok(Key::Char('\n')) = key {
+            break;
+        }
+    }
+
+    write!(raw_stdout, "{}", clear::All).unwrap();
+    raw_stdout.flush().unwrap();
+}
+
+fn pause_screen() -> Option<String> {
+    let stdout = stdout();
+    let mut raw_stdout = stdout.into_raw_mode().unwrap();
+
+    write!(raw_stdout, "{}{}", clear::All, cursor::Hide).unwrap();
+
+    let pause_text = [
+        "Game Paused",
+        "",
+        "Press [C] to Continue, [R] to Restart, or [E] to Exit",
+    ];
+
+    display_text(&mut raw_stdout, &pause_text);
+
+    let stdin = stdin();
+    for key in stdin.keys() {
+        match key.unwrap() {
+            Key::Char('C') | Key::Char('c') => return Some("continue".to_string()),
+            Key::Char('R') | Key::Char('r') => return Some("restart".to_string()),
+            Key::Char('E') | Key::Char('e') => return Some("exit".to_string()),
+            _ => {}
+        }
+    }
+    None
+}
+
+fn game_over_screen() -> bool {
+    let stdout = stdout();
+    let mut raw_stdout = stdout.into_raw_mode().unwrap();
+
+    write!(raw_stdout, "{}{}", clear::All, cursor::Hide).unwrap();
+
+    let game_over_text = [
+        "GAME OVER",
+        "",
+        "Press [R] to Restart or [E] to Exit"
+    ];
+
+    display_text(&mut raw_stdout, &game_over_text);
+
+    let stdin = stdin();
+    for key in stdin.keys() {
+        match key.unwrap() {
+            Key::Char('R') | Key::Char('r') => return true,
+            Key::Char('E') | Key::Char('e') => return false,
+            _ => {}
+        }
+    }
+    false
+}
+
+fn game_loop() -> bool {
+    let (width, height) = termion::terminal_size().unwrap();
+    let grid_width = width - 2;
+    let grid_height = height - 3;
+
+
     let stdin = stdin();
     let mut stdin_keys = stdin.keys();
     let stdout = stdout();
@@ -114,38 +213,54 @@ fn game_loop() {
     let mut food_y = 1;
 
     let mut snake = VecDeque::new();
-    let snake_center_x = GRID_WIDTH / 2+ 1;
-    let snake_center_y = GRID_HEIGHT / 2 + 1;
+    let snake_center_x = grid_width / 2 + 1;
+    let snake_center_y = grid_height / 2 + 1;
     snake.push_back((snake_center_x, snake_center_y));
 
-    food_position(&mut food_x, &mut food_y, &snake);
-    game_grid(&mut raw_stdout);
+    food_position(&mut food_x, &mut food_y, &snake, grid_width, grid_height);
+    game_grid(&mut raw_stdout, grid_width, grid_height);
     draw_snake(&mut raw_stdout, &snake);
     draw_food(&mut raw_stdout, food_x, food_y);
 
     raw_stdout.flush().expect("unable to display output");
 
+    let mut direction = Direction::Right;
+
     loop {
         if let Some(Ok(key)) = stdin_keys.next() {
             match key {
-                Key::Up => snake_movement(&mut snake, Direction::Up),
-                Key::Down => snake_movement(&mut snake, Direction::Down),
-                Key::Left => snake_movement(&mut snake, Direction::Left),
-                Key::Right => snake_movement(&mut snake, Direction::Right),
-                Key::Ctrl('c') => break,
+                Key::Up if direction != Direction::Down => direction = Direction::Up,
+                Key::Down if direction != Direction::Up => direction = Direction::Down,
+                Key::Left if direction != Direction::Right => direction = Direction::Left,
+                Key::Right if direction != Direction::Left => direction = Direction::Right,
+                Key::Esc => {
+                    if let Some(choice) = pause_screen() {
+                        match choice.as_str() {
+                            "continue" => continue,
+                            "restart" => return true,
+                            "exit" => return false,
+                            _ => {}
+                        }
+                    }
+                }
+                Key::Ctrl('c') => return false,
                 _ => {}
             }
         }
 
-        consume_food(&mut snake, &mut food_x, &mut food_y, &mut raw_stdout);
+        snake_movement(&mut snake, direction);
 
-        if detect_collision(&snake) {
+        if detect_collision(&snake, grid_width, grid_height) {
             println!("GAME OVER");
-            break;
+            return false;
+        }
+
+        if snake.front() == Some(&(food_x, food_y)) {
+            consume_food(&mut snake, &mut food_y, &mut food_x, &mut raw_stdout, grid_width, grid_height);
         }
 
         write!(raw_stdout, "{}", clear::All).unwrap();
-        game_grid(&mut raw_stdout);
+        game_grid(&mut raw_stdout, grid_width, grid_height);
         draw_snake(&mut raw_stdout, &snake);
         draw_food(&mut raw_stdout, food_x, food_y);
 
@@ -156,39 +271,17 @@ fn game_loop() {
 }
 
 pub fn initialize_game() {
-    let stdout = stdout();
-    let mut raw_stdout = stdout.into_raw_mode().unwrap();
+    welcome_screen();
 
-    write!(raw_stdout, "{}{}", clear::All, cursor::Hide).unwrap();
-    write!(raw_stdout, "{}", cursor::Goto(1, 1)).unwrap();
+    loop {
+        if !game_loop(){
+            break;
+        }
 
-    let mut food_x = 1;
-    let mut food_y = 1;
-
-    let snake_center_x = GRID_WIDTH / 2 + 1;
-    let snake_center_y = GRID_HEIGHT / 2 + 1;
-    let mut snake: VecDeque<(u16, u16)> = VecDeque::new();
-    snake.push_back((snake_center_x, snake_center_y));
-
-    food_position(&mut food_x, &mut food_y, &snake);
-    game_grid(&mut raw_stdout);
-    draw_snake(&mut raw_stdout, &snake);
-    draw_food(&mut raw_stdout, food_x, food_y);
-
-    raw_stdout.flush().expect("unable to display output");
-
-    game_loop();
-}
-
-/// pause_game
-fn pause_game() {
-}
-
-/// end/quit game
-/// user presses esc to quit game abruptly, give option to restart or quit game
-/// diplay final score upon collision
-fn end_game() {
-    unimplemented!();
+        if !game_over_screen() {
+            break;
+        }
+    }
 }
 
 
